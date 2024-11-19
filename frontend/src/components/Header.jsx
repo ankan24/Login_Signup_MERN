26f7d87704862be94992c3305771731b3ec76090
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-19 14:33:17 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Header() {
   return (
     <div className="bg-blue-700 p-7 flex justify-between items-center">    
-        <h1 className="text-5xl text-white"><Link to="/">Ankan</Link></h1>
+        <h1 className="text-5xl text-white"><Link to="/">SHOP SPHERE</Link></h1>
         <div className="flex gap-10 text-white">
         {/* <a href="/login" className="border-gray-900 rounded m-0 p-5 bg-emerald-600 active:scale-90">Login</a> */}
         {/* <a href="/signup" className="border-gray-900 rounded m-0 p-5 bg-emerald-600 active:scale-90">Signup</a> */}
