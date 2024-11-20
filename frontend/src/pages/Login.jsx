516b57781b3d3043d91c68cc3876c431e9afaa16
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-20 15:45:15 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,8 @@ function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await axios.post("http://localhost:3000/login", {
+        // const response = await axios.post("http://localhost:3000/login", {
+        const response = await axios.post("https://login-signup-backend-zw2k.onrender.com/login",
             mail: email,
             pswd: password,
         });
